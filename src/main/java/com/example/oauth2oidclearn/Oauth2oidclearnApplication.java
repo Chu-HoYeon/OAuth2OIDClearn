11ff18aaf4a480f6a3cbf848e35b3d32215eb08e
Chu-HoYeon <chu.hoyeon.dev@gmail.com>
2025-07-27 04:52:51 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/oauth2oidclearn/Oauth2oidclearnApplication.java
@@ -0,0 +1,13 @@
+package com.example.oauth2oidclearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2oidclearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2oidclearnApplication.class, args);
+	}
+
+}
